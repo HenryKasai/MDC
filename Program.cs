39a5441c39f6496dd0b0eec446688dedc46038d3
HenryKasai <henrykasai1775@gmail.com>
2020-07-05 21:21:07 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool terminar = false;
+            int primeiro, segundo;
+            while (!terminar)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Insira os dois números que você deseja ver o MDC.");
+                Console.Write("Primeiro número: ");
+                bool primeirobool = Int32.TryParse(Console.ReadLine(), out primeiro);
+                Console.Write("Segundo número: ");
+                bool segundobool = Int32.TryParse(Console.ReadLine(), out segundo);
+
+
+                if (!primeirobool || !segundobool)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Valor inválido, pressione uma tecla para inserir outro valor.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    int resto = primeiro % segundo;
+                    int primeironovo = primeiro;
+                    int segundonovo = segundo;
+                    int restonovo = resto;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (primeirobool && segundobool && resto == 0)
+                    {
+                        Console.WriteLine($"O MDC de {primeiro} e {segundo} = {segundo}");
+                        Console.ResetColor();
+                        Console.WriteLine("Pressione uma tecla para finalizar o programa.");
+                        Console.ReadKey();
+                        terminar = true;
+                    }
+                    if (primeirobool && segundobool && resto != 0)
+                    {
+                        while (restonovo != 0)
+                        {
+                            primeironovo = segundonovo;
+                            segundonovo = restonovo;
+                            restonovo = primeironovo % segundonovo;
+                        }
+                        Console.WriteLine($"O MDC de {primeiro} e {segundo} = {segundonovo}");
+                        Console.ResetColor();
+                        Console.WriteLine("Pressione uma tecla para finalizar o programa.");
+                        Console.ReadKey();
+                        terminar = true;
+                    }
+                }
+            }
+        }
+    }
+}
